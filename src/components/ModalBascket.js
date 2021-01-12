9016a9d1d8a47ccf1ed5ef1e21d0b76d8a6c0ca9
Leonid <leoural66@yandex.ru>
2021-01-12 13:13:44 +0500

    create modal window

--- src/components/ModalBascket.js
+++ src/components/ModalBascket.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import ModalWindow from './ModalWindow';
+
 
 class ModalBascket extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { openWindow: false };
+        this.state = { show: false };
         this.handleOpenModalWindow = this.handleOpenModalWindow.bind(this);
+        this.handleCloseModalWindow = this.handleCloseModalWindow.bind(this);
     }
 
     handleOpenModalWindow() {
-        this.setState({ openWindow: true });
-        setTimeout(() => {
-            console.log('open -> true ', this.state);
-        }, 100);
-
+        this.setState({ show: true });
+    }
+    handleCloseModalWindow() {
+        this.setState({ show: false });
     }
 
     render() {
@@ -21,7 +21,17 @@ class ModalBascket extends React.Component {
         return (
             <>
                 <button onClick={this.handleOpenModalWindow}>Ваши покупки</button>
-                <ModalWindow showModal={this.state.openWindow} />
+                <div className="modal" style={{ display: this.state.show ? 'block' : 'none' }}>
+                    <div className="modal__shadow" onClick={this.handleCloseModalWindow}></div>
+                    <div className="modal_window">
+                        <h3>Modal Window</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus ut cupiditate possimus officiis assumenda suscipit rerum eum, nisi nulla magni repellat laborum quisquam dolorum ipsum voluptas inventore vero accusamus ratione minima distinctio. Voluptatum ullam est neque, modi numquam ut. Distinctio velit eveniet, aut ipsum quibusdam eos obcaecati eaque impedit iusto?</p>
+                        <div className="btn-block">
+                            <button className="btn-close" onClick={this.handleCloseModalWindow}>Close</button>
+                            <button className="btn-pay">To pay</button>
+                        </div>
+                    </div>
+                </div>
             </>
         );
     }
