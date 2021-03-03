0c3b8c844d9423ab715642f439f67a764fae8b86
Leonid <leoural66@yandex.ru>
2021-03-03 21:31:47 +0500

    мелкие правки

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,30 @@
 import React from 'react';
 import HeaderSearch from './HeaderSearch';
 import HeaderTotal from './headerTotal';
-import image_1 from '../image/flowers_2.png';
-import image_2 from '../image/flowers_0.png';
+// import image_1 from '../image/flowers_2.png';
+// import image_2 from '../image/flowers_0.png';
 
 
 class Header extends React.Component {
 
     //анимация букетиков. Летают по экрану
-    animationBlock() {
-        const point = document.querySelector('.animation_block');
-        const point2 = document.querySelector('.animation_block2');
-        setInterval(() => {
-            point.classList.add("invisible_block");
-            point2.classList.remove("invisible_block");
-            console.log('ADD');
-        }, 30000);
-        setInterval(() => {
-            point.classList.remove("invisible_block");
-            point2.classList.add("invisible_block");
-            console.log('REMOVE');
-        }, 60000);
-    }
+    // animationBlock() {
+    //     const point = document.querySelector('.animation_block');
+    //     const point2 = document.querySelector('.animation_block2');
+    //     setInterval(() => {
+    //         point.classList.add("invisible_block");
+    //         point2.classList.remove("invisible_block");
+    //         console.log('ADD');
+    //     }, 30000);
+    //     setInterval(() => {
+    //         point.classList.remove("invisible_block");
+    //         point2.classList.add("invisible_block");
+    //         console.log('REMOVE');
+    //     }, 60000);
+    // }
 
     componentDidMount() {
-        this.animationBlock();
+        // this.animationBlock();
     }
 
     render() {
