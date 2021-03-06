31acbeb2d6c8e12a32261241c520f2d56b7154b6
Leonid <leoural66@yandex.ru>
2021-03-06 22:17:12 +0500

    верстка основного сайта

---
+++ src/components/View.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//основной класс для сайта
+export default class View extends React.Component {
+
+    render() {
+        return (
+            <>
+
+            </>
+        )
+    }
+}
