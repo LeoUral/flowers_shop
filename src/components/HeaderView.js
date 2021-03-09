f071a11ebe4d21a66f1b0e62d82500add45f3387
Leonid <leoural66@yandex.ru>
2021-03-09 11:59:31 +0500

    Верстка главной страницы

---
+++ src/components/HeaderView.js
@@ -0,0 +1,14 @@
+import React from ' react';
+
+export default class HeaderView extends React.Component {
+
+    render() {
+        return (
+            <>
+                <section className="container" >
+
+                </section>
+            </>
+        )
+    }
+}
--- src/components/View.js
+++ src/components/View.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HeaderView from './HeaderView';
 
 //основной класс для сайта
 export default class View extends React.Component {
@@ -6,7 +7,7 @@ export default class View extends React.Component {
     render() {
         return (
             <>
-
+                <HeaderView />
             </>
         )
     }
