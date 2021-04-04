8861380bb7a539065613260e2be8535b7f5cab03
Leonid <leoural66@yandex.ru>
2021-04-04 14:04:57 +0500

    верстка

--- src/site/ViewHeaderRight.jsx
+++ src/site/ViewHeaderRight.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
-import image from '../image/flowers_0.png';
+import image1 from '../image/flowers_0.png';
 
 export default class ViewHeaderRight extends React.Component {
 
@@ -8,7 +8,7 @@ export default class ViewHeaderRight extends React.Component {
         return (
             <>
                 <div className="container-right">
-                    <Image src={image} fluid className="view-image" alt="image" />
+                    <Image src={image1} fluid className="view-image" alt="image" />
                 </div>
             </>
         )
